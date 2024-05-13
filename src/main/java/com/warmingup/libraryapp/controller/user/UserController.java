e5b2c64519f486d4ca6e3131f9b2a303a0f21800
Yongtaek Lee <itaekit@gmail.com>
2024-05-13 20:31:49 +0900

    MySQL 연결하여 유저 생성, 조회, 수정, 삭제 API 개발

--- src/main/java/com/warmingup/libraryapp/controller/user/UserController.java
+++ src/main/java/com/warmingup/libraryapp/controller/user/UserController.java
@@ -2,32 +2,71 @@ package com.warmingup.libraryapp.controller.user;
 
 import com.warmingup.libraryapp.domain.user.User;
 import com.warmingup.libraryapp.dto.user.request.UserCreateRequest;
+import com.warmingup.libraryapp.dto.user.request.UserUpdateRequest;
 import com.warmingup.libraryapp.dto.user.response.UserResponse;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.web.bind.annotation.*;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UserController {
 
-    private final List<User> userList = new ArrayList<>();
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @PostMapping("/user")
     public void saveUser(@RequestBody UserCreateRequest request) {
-        userList.add(new User(request.getName(), request.getAge()));
+        String sql = "INSERT INTO user (name, age) VALUES (?, ?)";
+        jdbcTemplate.update(sql, request.getName(), request.getAge());
     }
 
     @GetMapping("/user")
     public List<UserResponse> getUsers() {
-        List<UserResponse> userResponseList = new ArrayList<>();
-        for (int i = 0; i < userList.size(); ++i) {
-            userResponseList.add(new UserResponse(Long.valueOf(i + 1), userList.get(i)));
-        }
+        String sql = "SELECT * FROM user";
+        List<UserResponse> userResponseList = jdbcTemplate.query(sql, new RowMapper<UserResponse>() {
+            @Override
+            public UserResponse mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Long id = rs.getLong("id");
+                String name = rs.getString("name");
+                Integer age = rs.getInt("age");
+                return new UserResponse(id, name, age);
+            }
+        });
 
         return userResponseList;
+    }
+
+    @PutMapping("/user")
+    public void updateUser(@RequestBody UserUpdateRequest request) {
+        String findSql = "SELECT * FROM user WHERE id = ?";
+        boolean userNotExist = jdbcTemplate.query(findSql, (rs, rowNum) -> 0, request.getId()).isEmpty();
+
+        if (userNotExist) {
+            throw new IllegalArgumentException(String.format("존재하지 않는 ID(%f)입니다.", request.getId()));
+        }
+
+        String sql = "UPDATE user SET name = ? WHERE id = ?";
+        jdbcTemplate.update(sql, request.getName(), request.getId());
+    }
+
+    @DeleteMapping("/user")
+    public void deleteUser(@RequestParam String name) {
+        String findSql = "SELECT * FROM user WHERE name = ?";
+        boolean userNotExist = jdbcTemplate.query(findSql, (rs, rowNum) -> 0, name).isEmpty();
+
+        if (userNotExist) {
+            throw new IllegalArgumentException(String.format("존재하지 않는 이름(%s)입니다.", name));
+        }
+
+        String sql = "DELETE FROM user WHERE name = ?";
+        jdbcTemplate.update(sql, name);
     }
 }
---
+++ src/main/java/com/warmingup/libraryapp/dto/user/request/UserUpdateRequest.java
@@ -0,0 +1,19 @@
+package com.warmingup.libraryapp.dto.user.request;
+
+public class UserUpdateRequest {
+    private final Long id;
+    private final String name;
+
+    public UserUpdateRequest(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
